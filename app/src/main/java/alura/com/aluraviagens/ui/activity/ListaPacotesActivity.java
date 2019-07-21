027b88afdc7bbebc47ee849d291ab1b2903b78c5
ValteirEleuterio <valteir.ea@gmail.com>
2019-07-21 02:30:25 -0300

    Alura Viagens

---
+++ app/src/main/java/alura/com/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,15 @@
+package alura.com.aluraviagens.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import alura.com.aluraviagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
