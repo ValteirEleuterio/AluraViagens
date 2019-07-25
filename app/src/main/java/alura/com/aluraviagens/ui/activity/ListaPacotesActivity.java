11997624158f553d4092b49db7a9ea9422e8fb11
ValteirEleuterio <valteir.ea@gmail.com>
2019-07-24 23:31:13 -0300

    implementação do adapter

--- app/src/main/java/alura/com/aluraviagens/ui/activity/ListaPacotesActivity.java
+++ app/src/main/java/alura/com/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -1,15 +1,31 @@
 package alura.com.aluraviagens.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.List;
 
 import alura.com.aluraviagens.R;
+import alura.com.aluraviagens.dao.PacoteDAO;
+import alura.com.aluraviagens.model.Pacote;
+import alura.com.aluraviagens.ui.adapter.ListaPacotesAdapter;
 
 public class ListaPacotesActivity extends AppCompatActivity {
+    private final String TITULO_APPBAR = "Pacotes";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pacotes);
+
+        setTitle(TITULO_APPBAR);
+        configuraLista();
+    }
+
+    private void configuraLista() {
+        ListView listaDePacotes = findViewById(R.id.lista_pacotes_listview);
+        List<Pacote> pacotes = new PacoteDAO().lista();
+        listaDePacotes.setAdapter(new ListaPacotesAdapter(pacotes, this));
     }
 }
