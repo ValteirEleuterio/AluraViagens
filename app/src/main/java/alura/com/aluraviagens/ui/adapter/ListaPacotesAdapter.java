55bdf328088458edfa15afa81ec01e8dbff79f42
ValteirEleuterio <valteir.ea@gmail.com>
2019-07-25 22:51:49 -0300

    Criado ViewHolder para o adapter da lista de pacotes (otimiza a performance(+- igual o RecyclerView))

--- app/src/main/java/alura/com/aluraviagens/ui/adapter/ListaPacotesAdapter.java
+++ app/src/main/java/alura/com/aluraviagens/ui/adapter/ListaPacotesAdapter.java
@@ -13,9 +13,10 @@ import java.util.List;
 
 import alura.com.aluraviagens.R;
 import alura.com.aluraviagens.model.Pacote;
-import alura.com.aluraviagens.util.DiasUtil;
-import alura.com.aluraviagens.util.MoedaUtil;
-import alura.com.aluraviagens.util.ResourceUtil;
+
+import static alura.com.aluraviagens.util.DiasUtil.formataEmTexto;
+import static alura.com.aluraviagens.util.MoedaUtil.formataParaBrasileiro;
+import static alura.com.aluraviagens.util.ResourceUtil.devolveDrawable;
 
 public class ListaPacotesAdapter extends BaseAdapter {
 
@@ -45,39 +46,59 @@ public class ListaPacotesAdapter extends BaseAdapter {
 
     @Override
     public View getView(int posicao, View convertView, ViewGroup parent) {
-        View viewCriada = LayoutInflater.from(context)
-                .inflate(R.layout.item_pacote, parent, false);
+        View view;
+        ViewHolder holder;
+
+        if (convertView == null) {
+            view = LayoutInflater.from(context)
+                    .inflate(R.layout.item_pacote, parent, false);
+            holder = new ViewHolder(view);
+            view.setTag(holder);
+        } else {
+            view = convertView;
+            holder = (ViewHolder) view.getTag();
+        }
 
         Pacote pacote = pacotes.get(posicao);
 
-        mostraLocal(viewCriada, pacote);
-        mostraImagem(viewCriada, pacote);
-        mostraDias(viewCriada, pacote);
-        mostraPreco(viewCriada, pacote);
+        mostraLocal(holder, pacote);
+        mostraImagem(holder, pacote);
+        mostraDias(holder, pacote);
+        mostraPreco(holder, pacote);
 
-        return viewCriada;
+        return view;
     }
 
-    private void mostraPreco(View viewCriada, Pacote pacote) {
-        TextView preco = viewCriada.findViewById(R.id.item_pacote_preco);
-        String moedaBrasileira = MoedaUtil.formataParaBrasileiro(pacote.getPreco());
-        preco.setText(moedaBrasileira);
+    private void mostraPreco(ViewHolder holder, Pacote pacote) {
+        String moedaBrasileira = formataParaBrasileiro(pacote.getPreco());
+        holder.preco.setText(moedaBrasileira);
     }
 
-    private void mostraDias(View viewCriada, Pacote pacote) {
-        TextView dias = viewCriada.findViewById(R.id.item_pacote_dias);
-        String diasTexto = DiasUtil.formataEmTexto(pacote.getDias());
-        dias.setText(diasTexto);
+    private void mostraDias(ViewHolder holder, Pacote pacote) {
+        String diasTexto = formataEmTexto(pacote.getDias());
+        holder.dias.setText(diasTexto);
     }
 
-    private void mostraImagem(View viewCriada, Pacote pacote) {
-        ImageView imagem = viewCriada.findViewById(R.id.item_pacote_imagem);
-        Drawable drawableImagemPacote = ResourceUtil.devolveDrawable(context, pacote.getImagem());
-        imagem.setImageDrawable(drawableImagemPacote);
+    private void mostraImagem(ViewHolder holder, Pacote pacote) {
+        Drawable drawableImagemPacote = devolveDrawable(context, pacote.getImagem());
+        holder.imagem.setImageDrawable(drawableImagemPacote);
     }
 
-    private void mostraLocal(View viewCriada, Pacote pacote) {
-        TextView local = viewCriada.findViewById(R.id.item_pacote_local);
-        local.setText(pacote.getLocal());
+    private void mostraLocal(ViewHolder holder, Pacote pacote) {
+        holder.local.setText(pacote.getLocal());
+    }
+
+    private class ViewHolder {
+        TextView preco;
+        TextView dias;
+        ImageView imagem;
+        TextView local;
+
+        ViewHolder(View view) {
+            preco = view.findViewById(R.id.item_pacote_preco);
+            dias = view.findViewById(R.id.item_pacote_dias);
+            imagem = view.findViewById(R.id.item_pacote_imagem);
+            local = view.findViewById(R.id.item_pacote_local);
+        }
     }
 }
