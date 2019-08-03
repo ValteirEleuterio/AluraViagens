756a4ae1a7d76364d994c98ea8106c27dc38c682
ValteirEleuterio <valteir.ea@gmail.com>
2019-08-02 21:37:48 -0300

    TELA DE PAGAMENTO

--- app/src/main/java/alura/com/aluraviagens/ui/activity/PagamentoActivity.java
+++ app/src/main/java/alura/com/aluraviagens/ui/activity/PagamentoActivity.java
@@ -2,14 +2,34 @@ package alura.com.aluraviagens.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 import alura.com.aluraviagens.R;
+import alura.com.aluraviagens.model.Pacote;
+import alura.com.aluraviagens.util.MoedaUtil;
 
 public class PagamentoActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Pagamento";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagamento);
+
+        setTitle(TITULO_APPBAR);
+
+        Pacote pacoteSaoPaulo = new Pacote("São Paulo",
+                "sao_paulo_sp", 2, new BigDecimal("243.99"));
+
+        mostraPreco(pacoteSaoPaulo);
+    }
+
+    private void mostraPreco(Pacote pacoteSaoPaulo) {
+        TextView preco = findViewById(R.id.pagamento_preco_pacote);
+        String moedaBrasileira = MoedaUtil.formataParaBrasileiro(pacoteSaoPaulo.getPreco());
+        preco.setText(moedaBrasileira);
     }
 }
